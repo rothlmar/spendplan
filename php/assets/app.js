4cf11c7445ea583f1d146a9a948494c23343c85a
Mark Rothlisberger <rothlmar@gmail.com>
2014-05-05 00:37:18 +0100

    adds local file uploads

--- php/assets/app.js
+++ php/assets/app.js
@@ -554,6 +554,40 @@ app.controller(
 	};
     });
 
+app.directive('fileChooser', function($timeout) {
+    return {
+	template: '<input type="file" />',
+	link: function(scope, element, attrs) {
+	    element.on('change', function(event) {
+		var file = event.target.files[0];
+		var reader = new FileReader();
+		reader.onload = (function() {
+		    return function(e) {
+			var temp_data = angular.element.csv.toArrays(e.target.result);
+			var max_num_cols = 0;
+			angular.forEach(temp_data, function(val) {
+			    if (val.length > max_num_cols) {
+				max_num_cols = val.length;
+			    };
+			});
+			scope.newTransactions.data = temp_data;
+			scope.newTransactions.cols = [];
+			for (var ndx = 0; ndx < max_num_cols; ndx++) {
+			    // scope.newTransactions.cols.push(ndx);
+			    scope.newTransactions.cols.push('None');
+			};
+			// don't know why this works, something about making it async.
+			$timeout(function () {
+			    angular.element('#importModal').modal('show');
+			}, 10);
+		    }
+		})();
+		reader.readAsText(file);
+	    })
+	}
+    }
+});
+
 app.directive('dbChooser', function($http) {
     return {
 	// template: '<input type="dropbox-chooser" name="selected-file"'
@@ -561,7 +595,7 @@ app.directive('dbChooser', function($http) {
 	//     + 'id="db-chooser" />',
 	template: '<button class="btn btn-default btn-xs">Upload from Dropbox</button>',
 	link: function(scope, element, attrs) {
-	    console.log(element);
+	    // console.log(element);
 	    element.on('click', function() {
 		Dropbox.choose({
 		    success: function(files) {
